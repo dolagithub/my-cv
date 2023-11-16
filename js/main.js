fbd5c7aa4b48a5e3905ccf6b8fd6ae93c3482aae
dolagithub <dolarani399@gmail.com>
2023-11-16 20:36:53 +0600

    first commit

--- js/main.js
+++ js/main.js
@@ -1,22 +1,20 @@
 function opentab(name) {
-    const links = document.getElementsByClassName('link');
-    for (const link of links) {
-        link.classList.remove('active-link');
+	const links = document.getElementsByClassName("link");
+	for (const link of links) {
+		link.classList.remove("active-link");
+	}
+	const tabs = document.getElementsByClassName("tab-container");
+	for (const tab of tabs) {
+		tab.classList.remove("active-tab");
+	}
+	event.currentTarget.classList.add("active-link");
 
-    }
-    const tabs = document.getElementsByClassName('tab-container');
-    for (const tab of tabs) {
-        tab.classList.remove('active-tab');
-    }
-    event.currentTarget.classList.add('active-link');
-
-    document.getElementById(name).classList.add('active-tab');
+	document.getElementById(name).classList.add("active-tab");
 }
-const menu = document.getElementById('sidemenu');
+const menu = document.getElementById("sidemenu");
 function showmenu() {
-    menu.style.right = '0';
+	menu.style.right = "0";
 }
 function closemenu() {
-    menu.style.right = '-150px';
+	menu.style.right = "-150px";
 }
-
